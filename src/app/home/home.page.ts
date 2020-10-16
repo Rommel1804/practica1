4ce058527b71819afbdb9e2b2980a8a076ea1419
Rommel vargas <romking39@yahoo.es>
2020-10-16 15:17:22 -0600

    agragar login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -10,10 +11,18 @@ export class HomePage {
    resultado: string="-";
    num1: string="";
    num2: string="";
-  constructor() {}
+  
+   constructor(private router:Router) {}
+  
    calcular(){
     this.resultado=(parseInt(this.num1)+parseInt(this.num2)).toString();
 
   }
+
+
+  login() {
+    this.router.navigate(["/login"]);
+  }
+
   
 }
